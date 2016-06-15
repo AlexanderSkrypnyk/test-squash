490d934e66c506b082bca46f56e04ff28f2eaf86
Alex Tsap <komod.kharkiv.alex@gmail.com>
2016-06-15 17:07:32 +0300

    feat(create): create some code

--- assets/src/test.tsap.js
+++ assets/src/test.tsap.js
@@ -1,3 +1,25 @@
-/**
- * Created by alex on 6/15/16.
- */
+var arr = [];
+
+var n = 10000000;
+
+for (var i = 0; i < n; i++) {
+
+  var randomNum = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  var choosenNum = randomNum(-1, 1);
+  arr.push(choosenNum);
+}
+
+var file = '/home/alex/FBtest/LearnJS/data.json';
+var obj = {name: 'JP'};
+var fs = require('fs');
+fs.writeFile(file, JSON.stringify(arr), function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("JSON saved to " + file);
+  }
+  //записали сгенерированный масив в файл
+});
